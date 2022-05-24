53d75e8104db9f04e66642ad96adae89843726fc
Jason <jason@dotjson.net>
2022-05-23 21:38:55 -0600

    add state to upgrade

--- src/components/Upgrade.jsx
+++ src/components/Upgrade.jsx
@@ -4,9 +4,20 @@ class Upgrade extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			upgradeName,
-			upgradeCost,
-			upgradeCPM,
+			upgradeName: "default",
+			upgradeCost: 1,
+			upgradeCoinsPerInterval: 1,
+			intervalInMilliSeconds: 1000,
+			isShowing: false,
 		};
 	}
+	render() {
+		return (
+			<div>
+				<h1>{this.state.upgradeName}</h1>
+			</div>
+		);
+	}
 }
+
+export default Upgrade;
--- src/components/UpgradeMenu.jsx
+++ src/components/UpgradeMenu.jsx
@@ -1,3 +1,14 @@
 import React from "react";
+import Upgrade from "./Upgrade";
 
-class UpgradeMenu extends React.Component {}
+class UpgradeMenu extends React.Component {
+	render() {
+		return (
+			<div>
+				<Upgrade className="menu" />
+			</div>
+		);
+	}
+}
+
+export default UpgradeMenu;
