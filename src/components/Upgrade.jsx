9bb24f757a1dd24b3cc0e1625a86336f4529f911
Jason <jason@dotjson.net>
2022-05-23 21:27:08 -0600

    add upgrade and upgrade menu

---
+++ src/components/Upgrade.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Upgrade extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			upgradeName,
+			upgradeCost,
+			upgradeCPM,
+		};
+	}
+}
